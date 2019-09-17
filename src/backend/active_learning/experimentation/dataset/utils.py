2cdd50d7426235858cc7de68a5da4c46966bcb08
Guy <guy7145@gmail.com>
2019-09-17 17:33:17 +0300

    return of the shuffle_dataset

--- src/backend/active_learning/experimentation/dataset/utils.py
+++ src/backend/active_learning/experimentation/dataset/utils.py
@@ -2,9 +2,8 @@ import numpy as np
 
 
 def shuffle_dataset(X: np.array, y: np.array):
-    # perm = np.random.permutation(X.shape[0])
-    # return X[perm], y[perm]
-    return X, y
+    perm = np.random.permutation(X.shape[0])
+    return X[perm], y[perm]
 
 
 def empty_array_from_template(template_array):
